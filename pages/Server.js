36819daf044c614241cdb9bd6c318c75f557d1d8
unknown <U84506866@gmail.com>
2025-11-01 19:13:22 +0330

    changed version mismatch MySQL

---
+++ pages/Server.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Bpdch@rzt29",
+  database: "webpage",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+function Sign_Up(username, password) {
+  // You can implement registration logic here later
+}
